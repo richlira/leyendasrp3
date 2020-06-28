e21e3c1e9402732abbff8708f1a8b6999d973f0d
Ricardo Lira de la Vega <ricardodelavega@gmail.com>
2020-06-28 17:47:52 -0500

    Feature: Add navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { StartComponent } from './modules/start/component/start.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: '', component: StartComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,19 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './core/navbar/navbar.component';
+
+import { StartModule } from './modules/start/start.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    StartModule
   ],
   providers: [],
   bootstrap: [AppComponent]
