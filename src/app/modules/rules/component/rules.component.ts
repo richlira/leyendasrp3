eb9df90ec8a793b55f0fc1f93f79562cc03364b6
Rich Lira <ricardodelavega@gmail.com>
2020-06-30 00:25:50 -0500

    Feature: fixes assets

--- src/app/modules/rules/component/rules.component.ts
+++ src/app/modules/rules/component/rules.component.ts
@@ -22,7 +22,7 @@ export class RulesComponent {
       {
         title: 'Reglamento LSPD',
         caption: 'Reglamento LSPD',
-        image: 'assets/citizen-card.png',
+        image: 'assets/police-card.png',
         description: 'Las reglas para los policias de la ciudad',
         action: 'Leer reglamento',
         url: 'https://bit.ly/NormativaLSPD-LeyendasRP'
@@ -30,7 +30,7 @@ export class RulesComponent {
       {
         title: 'Reglamento EMS',
         caption: 'Reglamento EMS',
-        image: 'assets/citizen-card.png',
+        image: 'assets/ems-card.png',
         description: 'Las reglas de todos los medicos de la ciudad',
         action: 'Leer reglamento',
         url: 'https://docs.google.com/document/d/1-GSyALHEdWUgT2ODU5upElzfgHWo7PM_mAC9r5ouaXQ/edit'
@@ -38,7 +38,7 @@ export class RulesComponent {
       {
         title: 'Reglamento Atracos',
         caption: 'Reglamento Atracos',
-        image: 'assets/citizen-card.png',
+        image: 'assets/criminal-card.png',
         description: 'Las reglas para llevar atracos en la ciudad',
         action: 'Leer reglamento',
         url: 'https://docs.google.com/document/d/1_4_fxEF48HRMDifQ8xCXAi_A-BGwfnguEfxXlEs3vrY/edit#'
@@ -46,7 +46,7 @@ export class RulesComponent {
       {
         title: 'Reglamento Mecanicos',
         caption: 'Reglamento Mecanicos',
-        image: 'assets/citizen-card.png',
+        image: 'assets/mechanic-card.png',
         description: 'Las reglas para llevar el rol de mecanico',
         action: 'Leer reglamento',
         url: 'https://docs.google.com/document/d/1XTd8utmpwfKPs05IaoZDu0m8r6SRck7zR2VeesR8Htc/edit'
