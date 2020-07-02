176b236fcd916e599c4d8d113eb3186534f9e6f4
Rich Lira <ricardodelavega@gmail.com>
2020-07-01 22:55:03 -0500

    Feature: adds banda/mafia rules

--- src/app/modules/rules/component/rules.component.ts
+++ src/app/modules/rules/component/rules.component.ts
@@ -50,6 +50,14 @@ export class RulesComponent {
         description: 'Las reglas para llevar el rol de mecanico',
         action: 'Leer reglamento',
         url: 'https://docs.google.com/document/d/1XTd8utmpwfKPs05IaoZDu0m8r6SRck7zR2VeesR8Htc/edit'
+      },
+      {
+        title: 'Reglamento Bandas/Mafia',
+        caption: 'Reglamento Bandas y Mafia',
+        image: 'assets/mafia-card.png',
+        description: 'Las reglas para el rol de las bandas y las mafias',
+        action: 'Leer reglamento',
+        url: 'https://docs.google.com/document/d/14poo-BVZdsuqJrwu0wbKltZ88hnkwJaD4pEgw-L3xts/edit?usp=sharing'
       }
     ]
   }
